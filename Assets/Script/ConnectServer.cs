95945770b2afa1007dd1f32c073c1113a1177be3
say-annyeong <sinjemen07@gmail.com>
2025-09-07 16:33:10 +0900

    재수정

---
+++ Assets/Script/ConnectServer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectServer : MonoBehaviour
+{
+    private UnityTcpStream unityTcpStream;
+
+    private void Start()
+    {
+        unityTcpStream = GameObject.Find("NetworkManager").GetComponent<UnityTcpStream>();
+
+        if (unityTcpStream == null)
+        {
+            Debug.LogError("NetworkManager not found or UnityTcpStream component is missing.");
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 예시: 서버에 연결 요청
+            string sendType = "name";
+            string command = "get";
+            string uuid = System.Guid.NewGuid().ToString();
+            Dictionary<string, object> sendData = new Dictionary<string, object>
+            {
+                { "name", "와샌주" }
+            };
+
+            string jsonString = TcpStreamConverter.GenerateJsonString(sendType, command, uuid, sendData, null);
+            unityTcpStream.ServerWrite(jsonString);
+        }
+    }
+}
---
+++ Assets/Script/TcpStreamData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct TcpStreamData
+{
+    public string send_type; // uuid, name, connect
+    public string command; // check, get, send, request, both
+    public string uuid; // UUID
+    public Dictionary<string, object> send_data; // 전송할 데이터
+    public Dictionary<string, object> request_data; // 요청할 데이터
+}
