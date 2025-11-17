25d65700bef1887b5d15f5abbfb9393d501fd9fa
Rorum <phoo8651@gmail.com>
2025-11-17 19:59:39 +0900

    asdf

--- Assets/Script/UnityTcpStream.cs
+++ Assets/Script/UnityTcpStream.cs
@@ -87,6 +87,13 @@ public class UnityTcpStream : MonoBehaviour
         }
     }
 
+    private string InitializeedName(string name, string command, string uuid)
+    {
+        Dictionary<string, object> send_data = new Dictionary<string, object>();
+        Dictionary<string, object> 
+        send_data.Add(uuid, "name");
+        TcpStreamData data = SendAndReceive("name", command, )
+    }
 
     public T NewGetData<T>(string name_space, string uuid)
     {
