dbdd0ed37cef99b87c19161fc6ae232347461351
say-annyeong <86085202+say-annyeong@users.noreply.github.com>
2025-11-21 20:56:04 +0900

    Refactor TcpStreamData struct comments and types
    Updated comments for clarity and corrected type annotations.

--- Assets/Script/TcpStreamData.cs
+++ Assets/Script/TcpStreamData.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-[Serializable]
 public struct TcpStreamData
 {
     public string send_type; // uuid, name, connect
     public string command; // check, get
     public string uuid; // UUID
-    public Dictionary<string, Dictionary<string, objct>> send_data; // 전송할 데이터
-    public Dictionary<string, object> request_data; // 요청할 데이터
+    public Dictionary<string, Dictionary<string, objct>> send_data; // Map<UUID, Map<DataName, Data>>
+    public Dictionary<string, object> request_data; // Map<UUID, List<DataName>>
 }
